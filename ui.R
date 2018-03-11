d1537eb4ae173a8eea957d235d72d1f4dd3a802e
abhraimrb <30985012+abhraimrb@users.noreply.github.com>
2018-03-11 12:07:33 +0530

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,21 @@
+
+
+library(shiny)
+
+shinyUI(fluidPage(
+  titlePanel("Power Calculator"),
+  sidebarLayout(sidebarPanel( h1("Resistance of Circuit"),  numericInput("obs1", "Value in ohms:", 20, min = 1, max = 100000)
+                              ,h1("Inductive Reactance"),numericInput("obs2", "Value in ohms", 25, min = 1, max = 100000),
+                              h1("Capacitative reactance"), numericInput("obs3", "Value in ohms", 10, min = 1, max = 100000)
+  ),
+  
+ 
+  mainPanel(
+    tabsetPanel(
+      tabPanel(h4("Power Calculation"),h3("Active Power in Watts"),textOutput("text1"),h3("Reactive Power in VAR"),textOutput("text2"),h3("Apparent Power in VA"),textOutput("text3"),h3("Impedance of circuit in Ohms"),textOutput("text4")),
+      tabPanel("Documentation",includeHTML("pow_doc.html"))
+    )
+  )
+  
+  ))
+)
